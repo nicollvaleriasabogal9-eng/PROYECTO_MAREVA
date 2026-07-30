3f452fff042b10062932feed8dae63d54a736a4f
naamdvl <andresaroca721@gmail.com>
2026-07-29 20:55:41 -0500

    Agregamos las urls gets

--- Proyecto Mareva/backend/routes/auth_routes.py
+++ Proyecto Mareva/backend/routes/auth_routes.py
@@ -1,14 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from controllers.auth_controllers import AuthController
 
 auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.route("/registro", methods=["POST"])
-def registro():
+def funcion_registro():
+    print("Se accedio al registro correcamente")
     return AuthController.registrar_usuario()
 
+@auth_bp.route("/registro", methods=["GET"])
+def mostrar_registro():
+    print("Se accedio al registro correcamente")
+    return render_template("principal/registro.html")
+
+@auth_bp.route("/login", methods=["GET"])
+def mostrar_login():
+    print("Se accedio al login correcamente")
+    return render_template("principal/login.html")
+
 @auth_bp.route("/login", methods=["POST"])
-def registro():
-    return AuthController.registrar_usuario()
+def funcion_login():
+    print("Se accedio al login correcamente")
+    return AuthController.iniciar_sesion()
 
 
