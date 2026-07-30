df05743c7bec8d7b5928c5f78ef9f04f77237e8c
naamdvl <andresaroca721@gmail.com>
2026-07-29 20:56:18 -0500

    Agregamos la url de la carpeta templates

--- Proyecto Mareva/backend/main.py
+++ Proyecto Mareva/backend/main.py
@@ -2,7 +2,8 @@ from flask import Flask
 from routes.auth_routes import auth_bp
 
 
-main = Flask(__name__)
+main = Flask(__name__, template_folder="../frontend/templates", static_folder="../frontend/static")
+main.secret_key = "mareva_secret_2026"
 
 main.register_blueprint(auth_bp)
 
