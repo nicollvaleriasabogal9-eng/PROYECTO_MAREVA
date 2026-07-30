7335585a889c7f1bd14416dc711e55c9c5c8df05
naamdvl <andresaroca721@gmail.com>
2026-07-29 20:53:23 -0500

    Agregamos el modelo del cliente

---
+++ Proyecto Mareva/backend/models/cliente.py
@@ -0,0 +1,15 @@
+class Cliente:
+    def __init__(self, nombre, apellido, tipo_documento, numero_documento, telefono, correo, contrasena, rol, codigo_referido, fecha_registro, estado, intentos_fallidos, bloqueado_hasta):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.tipo_documento = tipo_documento
+        self.numero_documento = numero_documento
+        self.telefono = telefono
+        self.correo = correo
+        self.contrasena = contrasena
+        self.rol = rol
+        self.codigo_referido = codigo_referido
+        self.fecha_registro = fecha_registro
+        self.estado = estado
+        self.intentos_fallidos = intentos_fallidos
+        self.bloqueado_hasta = bloqueado_hasta
