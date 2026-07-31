d6b9b72ee2a831c4f0a237042de146ebbbf1e588
naamdvl <andresaroca721@gmail.com>
2026-07-30 19:02:51 -0500

    Mejoras de las funciones para la conexion

--- Proyecto Mareva/backend/config/conexion.py
+++ Proyecto Mareva/backend/config/conexion.py
@@ -1,16 +1,18 @@
 import psycopg
 
 class Conexion():
-    
-    def conectar():
-        conexion = psycopg.connect(
-        host="localhost",
-        dbname="dbmareva",
-        user="postgres",
-        password="1234",
-        port=5432
-    )
-        return conexion
+
+    def __init__(self):
+        self.conexion = psycopg.connect(
+            host="localhost",
+            dbname="dbmareva",
+            user="postgres",
+            password="1234",
+            port=5432
+        )
+
+    def obtener_conexion(self):
+        return self.conexion
 
 
 
