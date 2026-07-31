faec3502af8eb6ced57e2878b35223ed29192761
naamdvl <andresaroca721@gmail.com>
2026-07-30 19:04:40 -0500

    Test para funciones del login y register

--- Proyecto Mareva/backend/services/auth_services.py
+++ Proyecto Mareva/backend/services/auth_services.py
@@ -1,8 +1,14 @@
-
+from repositories.auth_repository import AuthRepository
 class AuthServices():
+
+    def __init__(self):
+        self.repository = AuthRepository()
     
-    def registrar():
-        return
     
-    def iniciar_sesion():
-        return
+    def iniciar_sesion(self, correo, password):
+        usuario = self.repository.buscar_por_correo(correo)
+        if usuario == None:
+            return print("No se encontro un usuario con este correo")
+        else:
+            return print("el correo esta registrado")
+        
