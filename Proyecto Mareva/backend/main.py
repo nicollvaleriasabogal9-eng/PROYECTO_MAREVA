198ca28a07b4d607c51e6dde3576c8f9ab0061a1
naamdvl <andresaroca721@gmail.com>
2026-08-02 13:57:54 -0500

    se añadio la clave secreta para el manejo de session

--- Proyecto Mareva/backend/main.py
+++ Proyecto Mareva/backend/main.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from routes.auth_routes import auth_bp
+from routes.home_routes import home_bp
+from routes.paquetes_routes import paquetes_bp
 
 
 main = Flask(__name__, template_folder="../frontend/templates", static_folder="../frontend/static")
+
 main.secret_key = "mareva_secret_2026"
 
+main.register_blueprint(paquetes_bp)
 main.register_blueprint(auth_bp)
+main.register_blueprint(home_bp)
 
 
 if __name__ == "__main__":
