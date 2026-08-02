5aeb5db4448c0f8b9dcf750a75f648e97ec89786
naamdvl <andresaroca721@gmail.com>
2026-08-02 13:59:31 -0500

    Agregamos la verificacion de credenciales para el inicio de sesion

--- Proyecto Mareva/backend/services/auth_services.py
+++ Proyecto Mareva/backend/services/auth_services.py
@@ -6,9 +6,17 @@ class AuthServices():
     
     
     def iniciar_sesion(self, correo, password):
+
         usuario = self.repository.buscar_por_correo(correo)
-        if usuario == None:
+        
+        if usuario is None:
             return print("No se encontro un usuario con este correo")
         else:
-            return print("el correo esta registrado")
+            if password == usuario.password:
+                print("Inicio Sesion correctamente")
+                return usuario
+            else:
+                print("La contraseña ingresada es incorrecta")
+            
+
         
