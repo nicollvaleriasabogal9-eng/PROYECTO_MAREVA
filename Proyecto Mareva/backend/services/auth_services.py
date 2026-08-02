54c86b25e63518f0871e23e89c92d6e267d20115
naamdvl <andresaroca721@gmail.com>
2026-08-02 17:34:44 -0500

    Se agrego la funcion para el registro

--- Proyecto Mareva/backend/services/auth_services.py
+++ Proyecto Mareva/backend/services/auth_services.py
@@ -3,12 +3,26 @@ class AuthServices():
 
     def __init__(self):
         self.repository = AuthRepository()
-    
-    
+
+    def registrar_usuario(self, nombre, apellido, tipo_documento, numero_documento, telefono, codigo, correo, password):
+
+        usuario = self.repository.guardar_usuario(
+            nombre,
+            apellido,
+            tipo_documento,
+            numero_documento,
+            telefono,
+            codigo,
+            correo,
+            password
+        )
+
+        return True
+
     def iniciar_sesion(self, correo, password):
 
         usuario = self.repository.buscar_por_correo(correo)
-        
+
         if usuario is None:
             return print("No se encontro un usuario con este correo")
         else:
