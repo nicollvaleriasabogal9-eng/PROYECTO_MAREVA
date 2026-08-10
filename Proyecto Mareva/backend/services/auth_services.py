533848e3f478e9b11aa3e5d42ab98dab1eeb6a38
naamdvl <andresaroca721@gmail.com>
2026-08-09 20:55:39 -0500

    Agregamos algunos services para la autenticacion

--- Proyecto Mareva/backend/services/auth_services.py
+++ Proyecto Mareva/backend/services/auth_services.py
@@ -1,12 +1,24 @@
 from repositories.auth_repository import AuthRepository
+from werkzeug.security import check_password_hash
+
+
 class AuthServices():
 
     def __init__(self):
         self.repository = AuthRepository()
 
-    def registrar_usuario(self, nombre, apellido, tipo_documento, numero_documento, telefono, codigo, correo, password):
-
-        usuario = self.repository.guardar_usuario(
+    def registrar_usuario(
+        self,
+        nombre,
+        apellido,
+        tipo_documento,
+        numero_documento,
+        telefono,
+        codigo,
+        correo,
+        password
+    ):
+        resultado = self.repository.guardar_usuario(
             nombre,
             apellido,
             tipo_documento,
@@ -17,20 +29,16 @@ class AuthServices():
             password
         )
 
-        return True
+        return resultado
 
     def iniciar_sesion(self, correo, password):
+        # Obtiene el hash de la contraseña del usuario desde la base de datos
+        password_hash = self.repository.obtener_password_por_correo(correo)
 
-        usuario = self.repository.buscar_por_correo(correo)
+        if password_hash is None:
+            return None
+        # Verifica si la contraseña proporcionada coincide con el hash almacenado
+        if not check_password_hash(password_hash, password):
+            return None
 
-        if usuario is None:
-            return print("No se encontro un usuario con este correo")
-        else:
-            if password == usuario.password:
-                print("Inicio Sesion correctamente")
-                return usuario
-            else:
-                print("La contraseña ingresada es incorrecta")
-            
-
-        
+        return self.repository.buscar_por_correo(correo)
