f86ce83d544abdd0509f874ef8b2f79fac011fdb
naamdvl <andresaroca721@gmail.com>
2026-08-09 20:44:46 -0500

    Modificamos las credenciales de la conexion para que sean variables modificables

--- Proyecto Mareva/backend/config/conexion.py
+++ Proyecto Mareva/backend/config/conexion.py
@@ -1,16 +1,23 @@
 import psycopg
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 class Conexion():
 
+    #Las credenciales de la base de datos correspondiente se deben poner en el .env 
     def __init__(self):
-        self.conexion = psycopg.connect(
-            host="localhost",
-            dbname="dbmareva",
-            user="postgres",
-            password="1234",
-            port=5432
+        self.conexion = psycopg.connect( 
+            host=os.getenv("DB_HOST"),
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            port=os.getenv("DB_PORT")
         )
 
+    
     def obtener_conexion(self):
         return self.conexion
 
