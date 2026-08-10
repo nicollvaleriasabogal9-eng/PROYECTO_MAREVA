204babf17c2396c301ec8cfad45794bea30fbba8
naamdvl <andresaroca721@gmail.com>
2026-08-09 20:49:00 -0500

    Agregamos unos nuevos routes y modificamos otros

--- Proyecto Mareva/backend/routes/auth_routes.py
+++ Proyecto Mareva/backend/routes/auth_routes.py
@@ -5,24 +5,28 @@ auth_bp = Blueprint('auth', __name__)
 
 controller = AuthController()
 
+#Muestra el formulario de registro
 @auth_bp.route("/registro", methods=["GET"])
 def mostrar_registro():
-    print("Se accedio al registro correcamente")
+    print("Se accedio al GET del registro correcamente")
     return render_template("principal/registro.html")
 
+#Maneja el registro de un nuevo usuario
 @auth_bp.route("/registro", methods=["POST"])
 def funcion_registro():
-    print("Se accedio al registro correcamente")
+    print("Se accedio al POST del registro correctamente")
     return controller.registrar_usuario()
 
+#Muestra el formulario de login
 @auth_bp.route("/login", methods=["GET"])
 def mostrar_login():
-    print("Se accedio al login correcamente")
+    print("Se accedio al GET del login correctamente")
     return render_template("principal/login.html")
 
+#Maneja el inicio de sesión de un usuario
 @auth_bp.route("/login", methods=["POST"])
 def funcion_login():
-    print("Se accedio al login correcamente")
+    print("Se accedio al POST del login correctamente")
     return controller.iniciar_sesion()
 
 
--- Proyecto Mareva/backend/routes/home_routes.py
+++ Proyecto Mareva/backend/routes/home_routes.py
@@ -2,18 +2,21 @@ from flask import Blueprint, render_template, redirect, session, url_for
 
 home_bp = Blueprint('home', __name__)
 
+#Muestra la página de inicio
 @home_bp.route("/")
 def home():
     print("Se accedio al inicio correcamente")
     return render_template("principal/index.html")
 
+#Muestra la página de perfil para clientes
 @home_bp.route("/perfil")
 def perfil(): 
     if 'usuario' not in session:
-        return redirect('home.home')
+        return redirect(url_for('home.home'))
     return render_template("cliente/perfil.html")
 
+#Cierra la sesion del usuario y redirige a la página de inicio
 @home_bp.route('/logout')
 def logout():
-    session.clear()
-    return redirect(url_for('home.home'))
+    session.pop("usuario", None)
+    return render_template("principal/login.html")
