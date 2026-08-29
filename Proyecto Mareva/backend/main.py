d2d7f605e9f9b813a554b273b733ddc92c5ef63b
naamdvl <andresaroca721@gmail.com>
2026-08-29 17:13:55 -0500

    Se agrego el blueprint para los destinos

--- Proyecto Mareva/backend/main.py
+++ Proyecto Mareva/backend/main.py
@@ -3,6 +3,7 @@ from routes.auth_routes import auth_bp
 from routes.home_routes import home_bp
 from routes.paquetes_routes import paquetes_bp
 from routes.reserva_routes import reserva_bp
+from routes.destino_routes import destinos_bp
 from datetime import timedelta
 
 
@@ -13,6 +14,7 @@ main.secret_key = "mareva_secret_2026"
 main.register_blueprint(reserva_bp)
 main.register_blueprint(paquetes_bp)
 main.register_blueprint(auth_bp)
+main.register_blueprint(destinos_bp)
 main.register_blueprint(home_bp)
 main.permanent_session_lifetime = timedelta(days=30) 
 
