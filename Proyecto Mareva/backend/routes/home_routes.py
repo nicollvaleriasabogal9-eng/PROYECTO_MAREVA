edde486142554a38cd542020956fccf58584e663
naamdvl <andresaroca721@gmail.com>
2026-08-31 17:41:50 -0500

    se agrego nuevas rutas para la funcion del perfil

--- Proyecto Mareva/backend/routes/home_routes.py
+++ Proyecto Mareva/backend/routes/home_routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template, redirect, session, url_for
+from controllers.home_controllers import  HomeController
 
 home_bp = Blueprint('home', __name__)
 
+controller = HomeController()
 #Muestra la página de inicio
 @home_bp.route("/")
 def home():
@@ -13,7 +15,16 @@ def home():
 def perfil(): 
     if 'usuario' not in session:
         return redirect(url_for('home.home'))
-    return render_template("cliente/perfil.html")
+    return controller.perfil()
+    
+
+@home_bp.route("/perfil/historial/eliminar", methods=["POST"])
+def eliminar_historial():
+    return controller.eliminar_historial()
+
+@home_bp.route("/perfil/historial/aplicar/<int:id_busqueda>")
+def aplicar_filtros(id_busqueda):
+    return controller.aplicar_filtros(id_busqueda)
 
 #Cierra la sesion del usuario y redirige a la página de inicio
 @home_bp.route('/logout')
