55b1a5ec6a8a5e875ad08ce5ae1c3c90ab09bb1e
naamdvl <andresaroca721@gmail.com>
2026-08-31 17:42:37 -0500

    se agregaron nuevos manejos de rutas

--- Proyecto Mareva/backend/routes/reserva_routes.py
+++ Proyecto Mareva/backend/routes/reserva_routes.py
@@ -11,6 +11,32 @@ def mostrar_formulario(slug):
         return redirect(url_for("auth.mostrar_login", next=f"/reserva/{slug}"))
     return controller.mostrar_formulario(slug)
 
+@reserva_bp.route(
+    "/reserva/viajero/<int:id_viajero>/editar",
+    methods=["POST"]
+)
+def guardar_cambios_viajero(id_viajero):
+    return controller.guardar_cambios_viajero(id_viajero)
+
+@reserva_bp.route(
+    "/reserva/<int:id_reserva>/viajeros",
+    methods=["GET"]
+)
+def modificar_viajeros(id_reserva):
+    return controller.modificar_viajeros(id_reserva)
+
+@reserva_bp.route(
+    "/reservas/<int:id_reserva>/completar",
+    methods=["POST"]
+)
+def completar_reserva(id_reserva):
+
+    return controller.completar_reserva(id_reserva)
+
+@reserva_bp.route("/admin/reservas")
+def listar_reservas_admin():
+    return controller.listar_reservas_admin()
+
 #Funciones para manejar la disponibilidad de un paquete y confirmar la reserva
 @reserva_bp.route("/api/paquetes/<int:id_paquete>/disponibilidad", methods=["GET"])
 def disponibilidad(id_paquete):
